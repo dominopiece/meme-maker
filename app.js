4149e1acf420e2fb006e1ab2a144ece8f5de811d
dominopiece <dominopiece@gmail.com>
2023-02-02 13:59:10 +0900

    practice

---
+++ app.js
@@ -0,0 +1,39 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = 800;
+canvas.height = 800;
+
+// ctx.moveTo(50, 50);
+// ctx.lineTo(150, 50);
+// ctx.lineTo(150, 150);
+// ctx.lineTo(50, 150);
+// ctx.lineTo(50, 50);
+// ctx.fill();
+
+// // home
+// ctx.fillRect(200, 200, 50, 200);
+// ctx.fillRect(400, 200, 50, 200);
+// ctx.lineWidth = 2;
+// ctx.fillRect(300, 300, 50, 100);
+// ctx.fillRect(200, 200, 200, 20);
+// ctx.moveTo(200, 200);
+// ctx.lineTo(325, 100);
+// ctx.lineTo(450, 200);
+// // ctx.stroke();
+// ctx.fill()
+
+// // human 
+// ctx.fillRect(210, 200, 15, 100);
+// // ctx.fillRect(210 - 10, 200  - 10, 15, 100);
+// ctx.fillRect(350, 200, 15, 100);
+// ctx.fillRect(260, 200, 60, 200);
+
+// ctx.arc(290, 150, 50, 0, 2 * Math.PI);
+// ctx.fill();
+
+// ctx.beginPath();
+// ctx.fillStyle = "white "
+// ctx.arc(270, 150, 5, 1 * Math.PI, 2 * Math.PI);
+// ctx.arc(310, 150, 5, 1 * Math.PI, 2 * Math.PI);
+// ctx.arc(290, 150, 10, 2 * Math.PI, 1 * Math.PI);
+// ctx.fill();
