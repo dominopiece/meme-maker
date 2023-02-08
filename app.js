aa96d0ef176e654ef93d347efcff26f294ca0d0b
dominopiece <dominopiece@gmail.com>
2023-02-08 09:22:18 +0900

    callback hell

--- app.js
+++ app.js
@@ -477,20 +477,54 @@ ctx.lineWidth = 5;
 //     console.log("final");
 //   });
 
-const pr = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("OK");
-  }, 1000);
-});
+// const pr = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("OK");
+//   }, 1000);
+//   setTimeout(() => {
+//     reject("Reject");
+//   }, 1000);
+// });
 
-pr.then((result) => {
-  console.log(result);
-})
-  .catch((err) => {
-    console.log(err);
-  })
-  .finally(() => {
-    console.log("finally");
-  });
+// pr.then((result) => {
+//   console.log(result);
+// })
+//   .catch((err) => {
+//     console.log(err);
+//   })
+//   .finally(() => {
+//     console.log("finally");
+//   });
 
-  
+// // # callback hell
+// const f1 = (callback) => {
+//   setTimeout(function () {
+//     console.log("1번 주문 완료");
+//     callback();
+//   }, 1000);
+// };
+
+// const f2 = (callback) => {
+//   setTimeout(function () {
+//     console.log("1번 주문 완료");
+//     callback();
+//   }, 2000);
+// };
+
+// const f3 = (callback) => {
+//   setTimeout(function () {
+//     console.log("3번 주문 완료");
+//     callback();
+//   }, 3000);
+// };
+
+// console.log("시작");
+// f1(function () {
+//   f2(function () {
+//     f3(function () {
+//       console.log("끝");
+//     });
+//   });
+// });
+
+
